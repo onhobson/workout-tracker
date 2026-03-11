b69c83d2c977e148a18be2bd535cc2e13222111e
Owen Hobson <onhobson@gmail.com>
2026-03-11 18:07:15 -0400

    Make username column unique

--- app/db/models.py
+++ app/db/models.py
@@ -13,7 +13,7 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    username: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
 
