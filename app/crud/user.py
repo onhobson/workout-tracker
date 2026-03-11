6883f8003af6620f35cde05e087f3d549b13ade8
Owen Hobson <onhobson@gmail.com>
2026-03-11 18:07:53 -0400

    Add get_user_by_username in crud/user.py

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,11 +1,27 @@
-from typing import Sequence
-
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.db.models import User
+from app.schemas.user import UserCreate
 
 
 def get_user(user_id: int, db: Session) -> User | None:
     stmt = select(User).where(User.id == user_id)
     return db.scalar(stmt)
+
+
+def get_user_by_username(db: Session, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    return db.scalar(stmt)
+
+
+def create_user(user: UserCreate, db: Session) -> User | None:
+    new_user = User(
+        **user.model_dump()
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
