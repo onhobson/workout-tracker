00caab9fdd34aa507582e9c6e4bd58cfed91613b
Owen Hobson <onhobson@gmail.com>
2026-03-11 20:28:46 -0400

    Move sensitive info to .env file, import with config.py

--- app/auth/jwt.py
+++ app/auth/jwt.py
@@ -2,11 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 
-#TODO: Get SECRET_KEY from env variable
-SECRET_KEY = "48cb8435f9832d6a07f40509d776dac48d3e39731e5ef6aeb0327553c66a3051"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
+from app.core.config import settings
 
 def create_access_token(
     data: dict,
@@ -17,11 +13,11 @@ def create_access_token(
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
 def decode_access_token(token: str) -> dict:
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings() # type: ignore
