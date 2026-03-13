5d7f021c54c9e019b79203b890565d0488999720
Owen Hobson <onhobson@gmail.com>
2026-03-13 17:21:29 -0400

    Add dependency override for get_current_user

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,10 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.auth.dependencies import get_current_user
 from app.main import app
 from app.db.database import get_session
-from app.db.models import Base
+from app.db.models import Base, User
 
 engine = create_engine(
     "sqlite://",
@@ -37,14 +38,32 @@ def db_session():
 
 
 @pytest.fixture()
-def client(db_session):
+def client(db_session, test_user):
 
     def override_get_db():
         yield db_session
 
     app.dependency_overrides[get_session] = override_get_db
 
+    def override_get_current_user():
+        yield test_user
+
+    app.dependency_overrides[get_current_user] = override_get_current_user
+
     with TestClient(app) as client:
         yield client
 
     app.dependency_overrides.clear()
+
+
+@pytest.fixture()
+def test_user(db_session):
+    user = User(
+        username="testuser",
+        email="testuser@example.com",
+        hashed_password="fakehash"
+    )
+    db_session.add(user)
+    db_session.commit()
+    db_session.refresh(user)
+    return user
