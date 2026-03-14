43c1660a76aff75b0261b62c02adebd888b715da
Owen Hobson <onhobson@gmail.com>
2026-03-14 15:38:56 -0400

    Clean up create_user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,19 +3,18 @@ import pytest
 from fastapi.testclient import TestClient
 
 
-def create_user(client: TestClient, username="testuser"):
+def create_user(client: TestClient, username="username", email="user@example.com", password="12345"):
     return client.post(
         "/users",
         json={
             "username": username,
-            "email": f"{username}@example.com",
-            "password": "password"
+            "email": email,
+            "password": password
         }
     )
     
 
 class TestCreateUser:
-
     def test_create_user(self, client: TestClient):
         response = create_user(client)
 
@@ -23,35 +22,37 @@ class TestCreateUser:
 
         data = response.json()
 
-        assert data["username"] == "testuser"
-        assert data["email"] == "testuser@example.com"
+        assert data["username"] == "username"
+        assert data["email"] == "user@example.com"
 
     
-    def test_create_duplicate_user(self, client: TestClient):
+    @pytest.mark.parametrize(
+            "username,email",
+            [
+                ("username", "otheremail@example.com"),
+                ("newname", "user@example.com"),
+            ]
+    )
+    def test_create_user_unique_values(self, client: TestClient, username, email):
         create_user(client)
-        response = create_user(client)
-        assert response.status_code == 400
+        response = create_user(client, username=username, email=email)
+
+        assert response.status_code == 422
 
     
     @pytest.mark.parametrize(
             "username,email,password",
             [
-                ("", "test@example.com", "password"),
-                ("username", "", "password"),
-                ("username", "bademail", "password"),
-                ("username", "test@example.com", ""),
+                ("", "user1@example.com", "12345"),
+                ("     ", "user2@example.com", "12345"),
+                ("user3", "", "12345"),
+                ("user4", "     ", "12345"),
+                ("user5", "user5@example.com", ""),
+                ("user6", "user6@example.com", "   "),
             ]
     )
-    def test_create_user_invalid_input(self, client: TestClient, username, email, password):
-        response = client.post(
-            "/users",
-            json={
-                "username": username,
-                "email": email,
-                "password": password
-            }
-        )
+    def test_create_user_invalid_values(self, client: TestClient, username, email, password):
+        response = create_user(client, username, email, password)
 
         assert response.status_code == 422
 
-        
