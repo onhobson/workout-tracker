690679e0d38a467f81599546ae10534545a13af9
Owen Hobson <onhobson@gmail.com>
2026-03-13 20:12:57 -0400

    Fix potential circular import in auth/dependencies

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -6,17 +6,18 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt import InvalidTokenError
+from sqlalchemy.orm import Session
 
 from app.auth.jwt import decode_access_token
 from app.crud.user import get_user_by_username
 from app.db.models import User
-from app.dependencies import DbSession
+from app.db.database import get_session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
-    db: DbSession,
+    db: Annotated[Session, Depends(get_session)],
 ):
     """
     Decode JWT token and return authenticated user.
