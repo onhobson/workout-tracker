b012b1769f2b62990a491492e700002022911402
Owen Hobson <onhobson@gmail.com>
2026-03-14 17:45:11 -0400

    Add factories for equipment and exercise

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.db.models import User, WorkoutSession, Set
+from app.db.models import Equipment, Exercise, User, WorkoutSession, Set
 
 
 @pytest.fixture()
@@ -27,15 +27,16 @@ def workout_factory(db_session, test_user):
     return create_workout
 
 @pytest.fixture()
-def set_factory(db_session, workout_factory):
+def set_factory(db_session, workout_factory, exercise_factory):
 
     def create_set(**kwargs):
 
         workout = workout_factory()
+        exercise = exercise_factory()
 
         defaults = {
             "workout_id": workout.id,
-            "exercise_id": 1,
+            "exercise_id": exercise.id,
             "reps": 8,
             "weight": 135,
             "rest": 120
@@ -52,3 +53,51 @@ def set_factory(db_session, workout_factory):
         return new_set
     
     return create_set
+
+
+@pytest.fixture()
+def equipment_factory(db_session):
+    
+    def create_equipment(**kwargs):
+
+        defaults = {
+            "name": "Barbell",
+            "input_mode": "barbell"
+        }
+
+        defaults.update(**kwargs)
+
+        equipment = Equipment(**defaults)
+
+        db_session.add(equipment)
+        db_session.commit()
+        db_session.refresh(equipment)
+
+
+        return equipment
+    
+    return create_equipment
+
+
+@pytest.fixture()
+def exercise_factory(db_session, equipment_factory):
+    
+    def create_exercise(**kwargs):
+        equipment = equipment_factory()
+
+        defaults = {
+            "equipment_id": 1,
+            "name": "Bench Press"
+        }
+
+        defaults.update(**kwargs)
+
+        exercise = Exercise(**defaults)
+
+        db_session.add(exercise)
+        db_session.commit()
+        db_session.refresh(exercise)
+        
+        return exercise
+    
+    return create_exercise
