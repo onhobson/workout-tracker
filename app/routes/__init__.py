511ded7d66891d28b9b37fc4bd9c8d2cdea55fe0
Owen Hobson <onhobson@gmail.com>
2026-03-15 14:26:23 -0400

    Add equipment router to router import list

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from app.routes.auth import router as auth_router
+from app.routes.equipment import router as equipment_router
 from app.routes.workout import router as workout_router
 from app.routes.set import router as set_router
 from app.routes.user import router as user_router
 
 routers = [
     auth_router,
+    equipment_router,
     workout_router,
     set_router,
     user_router,
