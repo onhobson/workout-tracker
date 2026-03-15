0611ca9b8675efa3dd7716659f7f8e8a04459ec9
Owen Hobson <onhobson@gmail.com>
2026-03-15 16:57:24 -0400

    Add get endpoints for all muscle groups and muscle group by id

--- app/routes/muscles.py
+++ app/routes/muscles.py
@@ -2,6 +2,24 @@ from fastapi import APIRouter, HTTPException, status
 
 from app.crud import muscles as crud_muscles
 from app.dependencies import *
-from app.schemas.muscles import MuscleGroupRead, MuscleGroupWithExercises
+from app.schemas.muscles import MuscleGroupRead
 
-router = APIRouter(prefix="/sets", tags=["Sets"])
+router = APIRouter(prefix="/muscles", tags=["Muscles"])
+
+
+@router.get("/", response_model=list[MuscleGroupRead])
+def get_all_muscle_groups(db: DbSession):
+    return crud_muscles.get_all_muscle_groups(db)
+
+
+@router.get("/{muscle_id}", response_model=MuscleGroupRead)
+def get_muscle_group(muscle_id: int, db: DbSession):
+    muscle_group = crud_muscles.get_muscle_group(muscle_id, db)
+
+    if not muscle_group:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Muscle group with id: {muscle_id} not found"
+        )
+    
+    return muscle_group
