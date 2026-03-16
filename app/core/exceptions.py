fa36513acd87809c255492a335388f60c2fe28af
Owen Hobson <onhobson@gmail.com>
2026-03-15 20:59:34 -0400

    Add InvalidForeignKeyError custom exception

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -6,10 +6,20 @@ class DuplicateUserError(Exception):
         message = f"{field} already exists"
         super().__init__(message)
 
+
 class EmptyStringError(Exception):
     """Raised when a supplied required field contains an empty string."""
 
     def __init__(self, field: str):
         self.field = field
         message = f"{field} can not be empty"
+        super().__init__(message)
+
+
+class InvalidForeignKeyIDError(Exception):
+    """Raised when a foreign key id given in a request body does not correspond to an existing id in the database."""
+
+    def __init__(self, field: str):
+        self.field = field
+        message = f"No existing {field} found"
         super().__init__(message)
