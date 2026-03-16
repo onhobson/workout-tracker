beccbcbd7c74e4bd5bbc9602bb356fa3aa0290bf
Owen Hobson <onhobson@gmail.com>
2026-03-15 20:42:39 -0400

    Add exercise router to router list

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.auth import router as auth_router
 from app.routes.equipment import router as equipment_router
+from app.routes.exercise import router as exercise_router
 from app.routes.muscles import router as muscle_router
 from app.routes.workout import router as workout_router
 from app.routes.set import router as set_router
@@ -8,6 +9,7 @@ from app.routes.user import router as user_router
 routers = [
     auth_router,
     equipment_router,
+    exercise_router,
     muscle_router,
     workout_router,
     set_router,
