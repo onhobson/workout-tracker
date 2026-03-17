c7400139dbf77455a7b8389d971ccbed120423d5
Owen Hobson <onhobson@gmail.com>
2026-03-16 21:17:16 -0400

    Add db table creation in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
+
+from app.db.models import Base
+from app.db.database import engine
 from app.routes import routers
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
 
 for router in routers:
     app.include_router(router)
